c22650674492631b2c893a0b400674b616ecb5e9
Tevaen Kenerly <tevaenkenerly1@gmail.com>
2026-07-07 23:22:19 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
 
 const app = express();
 const PORT = 3000;
 
+const travelerRoutes = require('./app_server/routes/traveler');
+app.use('/', travelerRoutes);
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+app.set('view engine', 'hbs');
 
 app.listen(PORT, () => {
-  console.log(`Travlr Getaways app running at http://localhost:${PORT}`);
+    console.log(`Travlr Getaways app running at http://localhost:${PORT}`);
 });
